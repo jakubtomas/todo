4fa71f3a3171e3f63bc9ab23838aa253ab38bcc0
tomasjakub <tomasjakub21@gmail.com>
2023-07-03 18:49:26 +0200

    update

--- src/app/components/stepper/stepper.component.ts
+++ src/app/components/stepper/stepper.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-stepper',
@@ -11,14 +11,34 @@ export class StepperComponent implements OnInit {
   ngOnInit(): void {}
 
   firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required]
+    name: ['', Validators.required],
+    age: ['', [Validators.required, Validators.max(99), Validators.min(22)]]
   });
   secondFormGroup = this._formBuilder.group({
     secondCtrl: ['', Validators.required]
   });
   isLinear = false;
 
+  get getFirstFormControl() {
+    return this.firstFormGroup.controls;
+  }
   reset() {
     console.log('click reset');
+  }
+
+  firstNext() {
+    console.log('vypis');
+
+    //    console.log(this.firstFormGroup);
+  }
+
+  doneStepper() {
+    console.log('--------');
+    console.log(this.firstFormGroup.value);
+    console.log('--------');
+    console.log(this.secondFormGroup.value);
+    console.log(this.secondFormGroup);
+    console.log('--------');
+    console.log();
   }
 }
